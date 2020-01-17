e1d9fa504b55eeeb73239f9c966d738de42b6a9d
hanji <abhidhamma91@gmail.com>
2020-01-17 21:43:30 +0900

    img

--- src/Routes/Diary/DiaryPresenter.js
+++ src/Routes/Diary/DiaryPresenter.js
@@ -59,3 +59,77 @@ export default ({
     </Wrapper>
   );
 };
+
+// import React, { useEffect, useContext } from 'react';
+// import styled from 'styled-components';
+// import Tab from '../../Components/Tab';
+// import Survey from '../../Components/Survey';
+// import SurveyDescription from '../../Components/SurveyDescription';
+// import SelectQuestion from '../../Components/SelectQuestions';
+// import { DiaryContext, DiaryAction } from './DiaryContext';
+// import { useHistory } from 'react-router-dom';
+
+// const Wrapper = styled.div`
+//   display: grid;
+//   grid-template-rows: 7vh;
+// `;
+
+// export default ({
+//   myQuestion,
+//   allQuestion,
+//   updateMyQuestion,
+//   refetchMyQuestion,
+//   createMyDiary,
+//   handleDiary,
+//   handleQuestion,
+// }) => {
+//   let history = useHistory();
+//   const { state, dispatch } = useContext(DiaryContext);
+//   useEffect(
+//     () =>
+//       dispatch({
+//         type: DiaryAction.PRE_LOAD,
+//         allQuestion,
+//         myQuestion,
+//         refetchMyQuestion,
+//         createMyDiary,
+//         updateMyQuestion,
+//         handleDiary,
+//         handleQuestion,
+//       }),
+//     [
+//       allQuestion,
+//       createMyDiary,
+//       dispatch,
+//       handleDiary,
+//       handleQuestion,
+//       myQuestion,
+//       refetchMyQuestion,
+//       updateMyQuestion,
+//     ]
+//   );
+//   console.log('DiaryPresenter');
+//   return (
+//     <Wrapper>
+//       <Tab />
+//       {state.action === 'Description' && <SurveyDescription />}
+//       {state.action === 'SelectQuestion' && <SelectQuestion />}
+//       {state.action === 'Survey' &&
+//         (state.surveyCount === false ? (
+//           history.push('/me')
+//         ) : (
+//           <>
+//             <Survey
+//             // myQuestion={myQuestion}
+//             // surveyCount={surveyCount}
+//             // setSurveyCount={setSurveyCount}
+//             // createDiary={createDiary}
+//             // setAction={setAction}
+//             // diary={diary}
+//             // setDiary={setDiary}
+//             />
+//           </>
+//         ))}
+//     </Wrapper>
+//   );
+// };
